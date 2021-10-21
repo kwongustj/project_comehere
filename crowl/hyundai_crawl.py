385d88a99ee93306a0bd617e1aa563a0b9f203aa
Kwon HyeonSeo <kwongustj@gmail.com>
2021-10-21 20:38:25 +0900

    Implement: "각 점포 층수, 순번 추가 / [순번,점포 이름, 전화번호, 층수]- 순으로 엑셀에 저장 "

--- crowl/hyundai_crawl.py
+++ crowl/hyundai_crawl.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import openpyxl
+
+count = 0
+floor = 0
+wb = openpyxl.Workbook()
+ws1 = wb.active
+
+ws1.title = '현대 백화점 충청점'
+ws1.append(["순번","점포 이름","전화번호","층"])
 
 url = 'http://www.ehyundai.com/newPortal/DP/FG/FG000000_V.do?branchCd=B00147000'
 
@@ -8,8 +17,21 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
+    soup2 = soup.prettify()
+    result_list = soup2.split('\n')
 
-    for link in soup.find_all(attrs={'class': 'item brand-item'}):
+    for link in soup.find_all(attrs={'class': ['item brand-item', "num"]}):
         str = link.get_text(" ", strip=True)
-        print(str)
+        if len(str)==2:
+            floor = str
+            continue
+        elif len(str)==1: continue
+        else:
+            array = str.rsplit(" ",1)
+            count = count+1
+            # print(count)
+            # print(array[0])
+            # print(array[1])
+            ws1.append([count,array[0],array[1],floor])
 
+wb.save("hyundai_crawl.xlsx")

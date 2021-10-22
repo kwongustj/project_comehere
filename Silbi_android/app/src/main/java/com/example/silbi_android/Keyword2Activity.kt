208ce1c687157a38a3587499f6f1a2e7463b7a67
Kwon HyeonSeo <kwongustj@gmail.com>
2021-10-22 16:08:33 +0900

    Add: "안드로이드 ui 작성 완료"

--- Silbi_android/app/src/main/java/com/example/silbi_android/Keyword2Activity.kt
+++ Silbi_android/app/src/main/java/com/example/silbi_android/Keyword2Activity.kt
@@ -1,19 +1,21 @@
 package com.example.silbi_android
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
-class KeywordActivity: AppCompatActivity(){
+class Keyword2Activity: AppCompatActivity(){
+
     private val Button: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.btn1)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_keyword)
+        setContentView(R.layout.activity_keyword2)
 
         Button.setOnClickListener {
-            startActivity(Intent(this, Keyword2Activity::class.java))
+            startActivity(Intent(this, RecommendActivity::class.java))
         }
     }
 }
---
+++ Silbi_android/app/src/main/java/com/example/silbi_android/RecommendActivity.kt
@@ -0,0 +1,13 @@
+package com.example.silbi_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+class RecommendActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommand)
+    }
+}

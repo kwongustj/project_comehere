5e7590d7cc7254ad444855380e6b4fe854156e7e
Kwon HyeonSeo <kwongustj@gmail.com>
2021-11-26 06:44:01 +0900

    Add: "건물 선택 후 키워드 선택 넘어 갈 때 건물 이름도 함께 넘기기 구현"

--- Silbi_android/app/src/main/java/com/example/silbi_android/KeywordActivity.kt
+++ Silbi_android/app/src/main/java/com/example/silbi_android/KeywordActivity.kt
@@ -1,16 +1,23 @@
 package com.example.silbi_android
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import kotlinx.android.synthetic.main.activity_keyword.*
 
 class KeywordActivity: AppCompatActivity(){
+
     private val Button: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.btn1)
     }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyword)
+
+        buildingName.setText(intent.getStringExtra("building"))
 
         Button.setOnClickListener {
             startActivity(Intent(this, Keyword2Activity::class.java))

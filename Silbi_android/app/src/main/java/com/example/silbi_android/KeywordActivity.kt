58e8792327086c8f65cdb1ef77e5f8f0c2f5fe5f
Kwon HyeonSeo <kwongustj@gmail.com>
2021-11-26 17:01:12 +0900

    Add: " 평점 정렬하여 보여주는 화면 구성"

--- Silbi_android/app/src/main/java/com/example/silbi_android/KeywordActivity.kt
+++ Silbi_android/app/src/main/java/com/example/silbi_android/KeywordActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_keyword.*
 
 class KeywordActivity: AppCompatActivity(){
@@ -12,15 +13,27 @@ class KeywordActivity: AppCompatActivity(){
         findViewById<AppCompatButton>(R.id.btn1)
     }
 
+    private val Button1: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.btn2)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyword)
 
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
         buildingName.setText(intent.getStringExtra("building"))
 
         Button.setOnClickListener {
             startActivity(Intent(this, Keyword2Activity::class.java))
+        }
+
+        Button1.setOnClickListener {
+            bottomSheetDialog.show()
         }
     }
 }

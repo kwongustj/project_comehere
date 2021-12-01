4c87339774e2e9dec553eb201df8a21b4a3fad3a
Kwon HyeonSeo <kwongustj@gmail.com>
2021-12-02 02:16:30 +0900

    Refactor: "평점 / 키워드부분 따로 함수 분리"

--- Silbi_server/평점기준/flask_rate.py
+++ Silbi_server/평점기준/flask_rate.py
@@ -40,53 +40,28 @@ class hello(Resource):
         count = 0
         args_dict = request.args.to_dict()
         print(args_dict)
-        for key, value in args_dict.items():
-            if value == " ":
+
+        lst = list(args_dict.values())
+        df2 = pd.read_excel('keyword_select.xlsx')
+        df2_list = df2.values.tolist()
+        keyword2_list = []
+        submit = {}
+        dictdict = {}
+        count = 1
+        for i in df2_list:
+            if i[0] in lst:
+                print(keyword2_list)
+                a= i[0]
+                del i[0]
+                string = "".join(i)
+                dic1 = {"data":string}
+                dictdict["todo"+str(count)] = dic1
                 count = count + 1
-        if count > 2:
-            df = pd.read_excel('result.xlsx')
-            df_list_ = []
-            df_list = df.values.tolist()
-            for i in df_list:
-                if i[2] == "평점 없음":
-                    continue
-                else:
-                    df_list_.append(i)
-
-            df_list_.sort(reverse=True, key=lambda x: x[2])
-
-            df_list_result = []
-            for i in range(0, 10):
-                df_list_result.append(df_list_[i])
-                shop = df_list_result[i][1].split(' ')
-                df_list_result[i][1] = shop[0]
-            Todos = {"todo" + str(i): {"task": string[1]} for i, string in enumerate(df_list_result)}
-            print(Todos)
-            print(json.dumps(Todos))
-            print('')
-            print(json.dumps(Todos, ensure_ascii=False, indent=4))
-            return Todos
-        else:
-            lst = list(args_dict.values())
-            df2 = pd.read_excel('keyword_select.xlsx')
-            df2_list = df2.values.tolist()
-            keyword2_list = []
-            submit = {}
-            dictdict = {}
-            for i in df2_list:
-                if i[0] in lst:
-                    print(keyword2_list)
-                    a= i[0]
-                    del i[0]
-                    string = "".join(i)
-                    dic1 = {a:string}
-                    submit.update(dic1)
-                    print(submit)
-                    dictdict["1"] = submit
-                    print(dictdict)
-                    print(json.dumps(dictdict))
-                    print('')
-                    print(json.dumps(dictdict, ensure_ascii=False, indent=4))
+        print(dictdict)
+        print(json.dumps(dictdict))
+        print('')
+        print(json.dumps(dictdict, ensure_ascii=False, indent=4))
+        return dictdict
 
 
 

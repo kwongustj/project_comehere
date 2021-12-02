91312b9d7d26c8fa5e6b6226dd6ae5cb468f000f
Kwon HyeonSeo <kwongustj@gmail.com>
2021-12-02 18:44:14 +0900

    Add: "서버에서 받아온 키워드들 chipgroup으로 보여주기 구현 "

--- Silbi_android/app/src/main/java/com/example/silbi_android/Keyword2Activity.kt
+++ Silbi_android/app/src/main/java/com/example/silbi_android/Keyword2Activity.kt
@@ -2,11 +2,16 @@ package com.example.silbi_android
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
+import com.google.android.material.chip.ChipGroup
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import retrofit2.Call
@@ -20,8 +25,13 @@ class Keyword2Activity: AppCompatActivity(){
     lateinit var mRetrofit2: Retrofit
     lateinit var mRetrofitAPI2: RetrofitAPI2
     lateinit var mCallTodoList2: Call<JsonObject>
+    private val chipgroup: ChipGroup by lazy {
+        findViewById<ChipGroup>(R.id.chipgroup)
+    }
+    var selectedKeywordList = arrayListOf<String>(" ", " ", " ", " ")
     var array2 = arrayListOf<String>()
-    var array3 = arrayListOf<String>(" ", " ")
+    var array3 = arrayListOf<String>()
+
     private val Button: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.btn1)
     }
@@ -29,25 +39,62 @@ class Keyword2Activity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyword2)
 
-        if(intent.hasExtra("listString")) {
             var listString = intent.getStringExtra("listString")
             if (listString != null) {
                 val list = listString.split("  ")
                 for (i in list) {
                     array2.add(i)
                 }
-                Log.d("aaaa",array2[0])
+                setRetrofit2()
+                callTodoList2()
             }
-        // text_sub의 문구를 msg라는 키값을 가진 intent의 정보로 바꾸어준다. }
+
             Toast.makeText(this@Keyword2Activity, listString, Toast.LENGTH_SHORT).show()
-            setRetrofit2()
-            callTodoList2()
-        }
 
 
         Button.setOnClickListener {
             startActivity(Intent(this, RecommendActivity::class.java))
         }
+    }
+
+    fun onAddChip(view: Keyword2Activity, i: String) {
+        val chip = Chip(this)
+        chip.text = i
+        chip.setChipBackgroundColorResource(R.color.bg_chip_state_list)
+        val drawble = ChipDrawable.createFromAttributes(
+            this,
+            null,
+            0,
+            R.style.Widget_MaterialComponents_Chip_Choice
+        )
+        chip.setChipDrawable(drawble)
+        chip.chipBackgroundColor = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_checked), intArrayOf(android.R.attr.state_checked)
+            ),
+            intArrayOf(Color.rgb(220, 220, 220), Color.rgb(255, 215, 157))
+        )
+        chip.isCheckable = true
+        var check = chip.isChecked
+        chip.setOnClickListener {
+            if (check == true) {
+                Toast.makeText(this@Keyword2Activity, "빼", Toast.LENGTH_SHORT).show()
+                check = false
+                selectedKeywordList.remove(i)
+                selectedKeywordList.add(" ")
+
+            } else {
+                Toast.makeText(this@Keyword2Activity, "추가됨", Toast.LENGTH_SHORT).show()
+                check = true
+                if (selectedKeywordList.contains(" ") == true) {
+                    selectedKeywordList.remove(" ")
+                    selectedKeywordList.add(i)
+                } else {
+                    selectedKeywordList.add(i)
+                }
+            }
+        }
+        chipgroup.addView(chip)
     }
 
     private fun callTodoList2() {
@@ -61,7 +108,7 @@ class Keyword2Activity: AppCompatActivity(){
     }
 
     private val mRetrofitCallback2
-            =(object : Callback<JsonObject> {
+            = (object : Callback<JsonObject> {
         override fun onFailure(call: Call<JsonObject>, t: Throwable) {
             t.printStackTrace()
             Log.d(TAG, "에러입니다. => ${t.message.toString()}")
@@ -70,15 +117,23 @@ class Keyword2Activity: AppCompatActivity(){
 
         override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
             val result = response.body()
-            Log.d(TAG, "결과는 => $result")
 
             var mGson = Gson()
-            val dataParsed0 = mGson.fromJson(result, DataModel2.TodoInfo10::class.java)
-            array3.set(0, dataParsed0.todo1.data)
+            val  dataParsed0= mGson.fromJson(result, DataModel2.TodoInfo10::class.java)
+            array3.add(dataParsed0.todo1.data)
+            var list = dataParsed0.todo1.data.toString().split(", ")
+
+            for (i in list) {
+                onAddChip(this@Keyword2Activity, i)
+            }
             val dataParsed1 = mGson.fromJson(result, DataModel2.TodoInfo11::class.java)
-            array3.set(1, dataParsed1.todo2.data)
-            Log.d("값", array3[0])
-            Log.d("값2", array3[1])
+            list = dataParsed1.todo2.data.toString().split(", ")
+            array3.add( dataParsed1.todo2.data)
+
+
+            for (i in list) {
+                onAddChip(this@Keyword2Activity, i)
+            }
 
 
 
@@ -87,14 +142,12 @@ class Keyword2Activity: AppCompatActivity(){
 
 
     private fun setRetrofit2() {
-        //레트로핏으로 가져올 url설정하고 세팅
         mRetrofit2 = Retrofit
             .Builder()
             .baseUrl(getString(R.string.baseUrl))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        //인터페이스로 만든 레트로핏 api요청 받는 것 변수로 등록
         mRetrofitAPI2 = mRetrofit2.create(RetrofitAPI2::class.java)
     }
 
